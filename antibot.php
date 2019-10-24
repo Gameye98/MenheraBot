ba218d824cff4148f7ac676c21fee28e0a6fdbad
Cvar1984 <32727560+Cvar1984@users.noreply.github.com>
2019-10-24 19:10:32 +0700

    Fix indent

--- antibot.php
+++ antibot.php
@@ -1,35 +1,35 @@
 <?php
 if (!empty($_SERVER['HTTP_USER_AGENT'])) {
-			$userAgents = array(
-				'Googlebot',
-				'DuckDuckBot',
-				'Baiduspider',
-				'Exabot',
-				'SimplePie',
-				'Curl',
-				'OkHttp',
-				'SiteLockSpider',
-				'BLEXBot',
-				'ScoutJet',
-				'AdsBot Google Mobile',
-				'Googlebot Mobile',
-				'MJ12bot',
-				'Slurp',
-				'MSNBot',
-				'PycURL',
-				'facebookexternalhit',
-				'facebot',
-				'ia_archiver',
-				'crawler',
-				'YandexBot',
-				'Rambler',
-				'Yahoo! Slurp',
-				'YahooSeeker',
-				'bingbot'
-			);
-			if (preg_match('/' . implode('|', $userAgents) . '/i', $_SERVER['HTTP_USER_AGENT'])) {
-				header('HTTP/1.0 404 Not Found');
-				exit();
-			}
-			unset($userAgents);
+    $userAgents = array(
+        'Googlebot',
+        'DuckDuckBot',
+        'Baiduspider',
+        'Exabot',
+        'SimplePie',
+        'Curl',
+        'OkHttp',
+        'SiteLockSpider',
+        'BLEXBot',
+        'ScoutJet',
+        'AdsBot Google Mobile',
+        'Googlebot Mobile',
+        'MJ12bot',
+        'Slurp',
+        'MSNBot',
+        'PycURL',
+        'facebookexternalhit',
+        'facebot',
+        'ia_archiver',
+        'crawler',
+        'YandexBot',
+        'Rambler',
+        'Yahoo! Slurp',
+        'YahooSeeker',
+        'bingbot'
+    );
+    if (preg_match('/' . implode('|', $userAgents) . '/i', $_SERVER['HTTP_USER_AGENT'])) {
+        header('HTTP/1.0 404 Not Found');
+        exit();
+    }
+    unset($userAgents);
 }
